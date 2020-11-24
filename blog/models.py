69c755fc7b01b16f7e1ae60ffa37ce1173fda8b1
harshita.gupta@contractor.temenos.com <harshita.gupta@contractor.temenos.com>
2020-11-24 22:52:39 +0530

    Modified current timezone settings

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.utils import timezone
+#from django.utils import timezone
+import datetime
 from django.urls import reverse
 
 class Post(models.Model):
     title=models.CharField(max_length=100)
     content=models.TextField()
-    date=models.DateTimeField(default=timezone.now)
+    date=models.DateTimeField(default=datetime.datetime.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     
     def __str__(self):
